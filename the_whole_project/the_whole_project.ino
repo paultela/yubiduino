6c0885a92403aa3b68681dd65a4657b35b67df8c
Brandon Mills <mills.brandont@gmail.com>
2014-12-02 02:30:47 -0500

    The whole project works

--- the_whole_project/the_whole_project.ino
+++ the_whole_project/the_whole_project.ino
@@ -20,13 +20,16 @@
 #include <DS3231.h>
 #include <SPI.h>
 #include <SD.h>
+#include <sha1.h>
 
 #define BUTTON_PIN 42
 #define SD_PIN 4
 #define KEY_FILE "key.txt"
+#define INTERVAL 30
+#define TOKEN_LENGTH 6
 
 DS3231 rtc(SDA, SCL);
-String key = "";
+uint8_t key[20];
 int previousButtonState = HIGH;
 
 // Gets a UNIX timestamp from the RTC
@@ -42,6 +45,15 @@ int readInt(const char* prompt) {
   }
 
   return Serial.parseInt();
+}
+
+void printHash(uint8_t* hash, size_t len) {
+  int i;
+  for (i=0; i<len; i++) {
+    Serial.print("0123456789abcdef"[hash[i]>>4]);
+    Serial.print("0123456789abcdef"[hash[i]&0xf]);
+  }
+  Serial.println();
 }
 
 // Prompts to set the time on the RTC
@@ -72,7 +84,76 @@ void command() {
   } else if (command == "set time") {
     setTime();
   } else if (command == "key") {
-    Serial.println(key);
+    printHash(key, 20);
+    Serial.println();
+  }
+}
+
+// From https://code.google.com/p/google-authenticator/source/browse/libpam/base32.c
+int base32_decode(const uint8_t *encoded, uint8_t *result, int bufSize) {
+  int buffer = 0;
+  int bitsLeft = 0;
+  int count = 0;
+  for (const uint8_t *ptr = encoded; count < bufSize && *ptr; ++ptr) {
+    uint8_t ch = *ptr;
+    if (ch == ' ' || ch == '\t' || ch == '\r' || ch == '\n' || ch == '-') {
+      continue;
+    }
+    buffer <<= 5;
+
+    // Deal with commonly mistyped characters
+    if (ch == '0') {
+      ch = 'O';
+    } else if (ch == '1') {
+      ch = 'L';
+    } else if (ch == '8') {
+      ch = 'B';
+    }
+
+    // Look up one base32 digit
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+      ch = (ch & 0x1F) - 1;
+    } else if (ch >= '2' && ch <= '7') {
+      ch -= '2' - 26;
+    } else {
+      return -1;
+    }
+
+    buffer |= ch;
+    bitsLeft += 5;
+    if (bitsLeft >= 8) {
+      result[count++] = buffer >> (bitsLeft - 8);
+      bitsLeft -= 8;
+    }
+  }
+  if (count < bufSize) {
+    result[count] = '\000';
+  }
+  return count;
+}
+
+void totp(uint8_t* secret, const size_t len, const long time, char token[TOKEN_LENGTH]) {
+  long timestamp = time / INTERVAL;
+  uint8_t input[8] = { 0x00, 0x00, 0x00, 0x00 };
+  input[4] = (timestamp >> 24) & 0xff;
+  input[5] = (timestamp >> 16) & 0xff;
+  input[6] = (timestamp >> 8) & 0xff;
+  input[7] = timestamp & 0xff;
+  
+  Sha1.initHmac(secret, len);
+  Sha1.write(input, 8);
+  uint8_t* hash = Sha1.resultHmac();
+  
+  uint8_t offset = hash[19] & 0xf;
+  
+  unsigned long thing = ((hash[offset] & 0x7f) << 24)
+                      | ((hash[offset + 1] & 0xff) << 16)
+                      | ((hash[offset + 2] & 0xff) << 8)
+                      | ((hash[offset + 3] & 0xff));
+  
+  for (int i = TOKEN_LENGTH - 1; i >= 0; i--) {
+    token[i] = (thing % 10) + '0';
+    thing /= 10;
   }
 }
 
@@ -97,12 +178,16 @@ void setup() {
     Serial.println("SD card has no key.txt");
     return;
   }
+  String keyString = "";
   while (keyFile.available()) {
     char ch = keyFile.read();
     if (ch == ' ' || ch == '\n') continue;
-    key.concat(ch);
+    keyString.concat(ch);
   }
   keyFile.close();
+  uint8_t keyText[33];
+  keyString.toCharArray((char*)keyText, 33);
+  base32_decode(keyText, key, 20);
 
   // Set up the keyboard
   Keyboard.begin();
@@ -118,7 +203,9 @@ void loop() {
   if (Serial.available()) {
     command();
   } else if (buttonState == HIGH && previousButtonState == LOW) {
-    Keyboard.print(key);
+    char token[TOKEN_LENGTH];
+    totp(key, 20, getTimestamp(), token);
+    Keyboard.print(token);
   } else {
     delay(100);
   }
